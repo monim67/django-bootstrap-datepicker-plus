b871d3c6056145f0fcd9fd99aaa77058da2814a4
Munim Munna <6266677+monim67@users.noreply.github.com>
2023-07-02 23:33:41 +0600

    Fix Doesn't support pydantic v2.0 (just released today) #99

--- src/bootstrap_datepicker_plus/settings.py
+++ src/bootstrap_datepicker_plus/settings.py
@@ -3,8 +3,13 @@ import functools
 from typing import Any, Dict, Optional, Tuple
 
 from django.conf import settings as django_settings
-from pydantic import Field, validator
-from pydantic.env_settings import BaseSettings, SettingsSourceCallable
+
+try:
+    from pydantic import Field, validator
+    from pydantic.env_settings import BaseSettings, SettingsSourceCallable
+except:
+    from pydantic.v1.env_settings import BaseSettings, SettingsSourceCallable  # type: ignore
+    from pydantic.v1 import Field, validator  # type: ignore
 
 from .schemas import WidgetOptions, WidgetVariant
 

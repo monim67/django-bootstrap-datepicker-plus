9739ecc08a529951830f84d9753749ac0e3d6332
Abdul Monim <6266677+monim67@users.noreply.github.com>
2026-02-13 12:13:55 +0600

    Release 6.0.0

--- src/bootstrap_datepicker_plus/settings.py
+++ src/bootstrap_datepicker_plus/settings.py
@@ -65,7 +65,7 @@ class WidgetSettings(BaseSettings):
         "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
     )
     app_static_url: str = (
-        "https://cdn.jsdelivr.net/gh/monim67/django-bootstrap-datepicker-plus@5.0.6/src/bootstrap_datepicker_plus/static/bootstrap_datepicker_plus/"
+        "https://cdn.jsdelivr.net/gh/monim67/django-bootstrap-datepicker-plus@4c7f504/src/bootstrap_datepicker_plus/static/bootstrap_datepicker_plus/"
     )
     debug: bool = Field(default_factory=lambda: getattr(django_settings, "DEBUG", True))
 
